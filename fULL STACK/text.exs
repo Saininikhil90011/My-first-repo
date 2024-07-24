afc6941239e9dd0680e153be8e4b319851422597
Saininikhil90011 <saininikhil90011@gmail.com>
2024-07-24 14:35:54 +0530

    new

---
+++ fULL STACK/text.exs
@@ -0,0 +1,42 @@
+  =>>Full Stack<<==
+
+<!-- frontend = > html, css,  js, react js,  and Bootstrap
+Backend => node js, express js
+Database => Mongo DB,  SQl -->
+
+
+tag  , element , attributes
+
+types of tag
+h1 tag
+
+HTML(hyper text markup language)
+
+
+Heading=> h1,h2,h3,h4,h5,h6
+Paragraf => P
+Ankertag => <a herf ="" , target="_blank" ></a>
+<br> = > line break 
+<hr> = > underline (____________________________________________________)
+
+<tr>=> row
+1.
+
+list=> there are two type
+1. order list 
+>>>type= counting, alphabat,roman
+
+2. unorder list 
+>>>>non
+
+2.
+  
+
+
+
+
+
+CSS(cascading style sheets) = >  there are three type of css
+1. Inline css
+2. Internal css
+3. Extranal css
